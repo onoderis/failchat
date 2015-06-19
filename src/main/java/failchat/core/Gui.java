dd9c539df970eeb20b1e561a46859e77bae998ef
sph <fail0003@gmail.com>
2015-06-19 09:18:14 +0300

    Links opening in default browser

--- src/main/java/failchat/core/Gui.java
+++ src/main/java/failchat/core/Gui.java
@@ -1,16 +1,21 @@
 package failchat.core;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
 public class Gui extends Application {
 
     private static final Logger logger = Logger.getLogger(MessageManager.class.getName());
+    private static final Path skinsPath = Bootstrap.workDir.resolve("skins");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -20,6 +25,18 @@ public class Gui extends Application {
         Scene webScene = new Scene(webView, 350, 500);
 
         webEngine.load(Bootstrap.workDir.resolve("skins/default/default.html").toAbsolutePath().toUri().toURL().toString());
+
+        //url opening interceptor
+        webEngine.locationProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
+                if (!newValue.contains(skinsPath.toAbsolutePath().toString())) {
+                    logger.fine("Opening url: " + newValue);
+                    Platform.runLater(() -> webEngine.getLoadWorker().cancel());
+                    getHostServices().showDocument(newValue);
+                }
+            }
+        });
 
         primaryStage.setScene(webScene);
         primaryStage.show();
