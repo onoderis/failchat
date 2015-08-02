8291c3c532aff611b799466ee4f8372f83e7cfd4
onoderis <onoderis@users.noreply.github.com>
2015-08-02 10:43:39 +0300

    Proper bbcode handling in sc2tv announce messages

--- src/main/java/failchat/sc2tv/BBCodeHandler.java
+++ src/main/java/failchat/sc2tv/BBCodeHandler.java
@@ -14,29 +14,30 @@ import java.util.regex.Pattern;
  */
 
 public class BBCodeHandler implements MessageHandler {
-    //иногда ссылки приходят в другом виде: [url=http://i.imgur.com/xxx.png]http://i.imgur.com/xxx.png[/url]
-    private static Pattern bbCodeUrlPattern = Pattern.compile("\\[url=(.+)\\]\\1\\[\\/url\\]");
-    private static HashMap<String, String> rules = new HashMap<String, String>() {{
-        put("\\[b\\]", "");
-        put("\\[/b\\]", "");
-        put("\\[url\\]", "");
-        put("\\[/url\\]", "");
+    private static String announcePattern = "\\[url=.+?\\](.+?)\\[\\/url\\]";
+    private static HashMap<Pattern, String> rules = new HashMap<Pattern, String>() {{
+        put(Pattern.compile("\\[url=(.+)\\]\\1\\[\\/url\\]"), "\1"); //иногда ссылки приходят в виде [url=http://xxx.x]http://xxx.x[/url]
+        put(Pattern.compile("\\[b\\]"), "");
+        put(Pattern.compile("\\[/b\\]"), "");
+        put(Pattern.compile("\\[url\\]"), "");
+        put(Pattern.compile("\\[/url\\]"), "");
     }};
 
     @Override
     public void handleMessage(Message message) {
-        Matcher m = bbCodeUrlPattern.matcher(message.getText());
-        int position = 0;
-        while (m.find(position)) {
-            position = m.start();
-            message.setText(m.replaceFirst(m.group(1)));
-            m = bbCodeUrlPattern.matcher(message.getText());
+        //announce or donation
+        if (message.getAuthor().equals("SC2TV")) {
+            message.setText(message.getText().replaceAll(announcePattern, "\1"));
         }
 
+        //rules processing
         String messageText = message.getText();
-        Set<String> set = rules.keySet();
-        for (String pattern : set) {
-            messageText = messageText.replaceAll(pattern, rules.get(pattern));
+        Set<Pattern> patternSet = rules.keySet();
+        for (Pattern pattern : patternSet) {
+            Matcher m = pattern.matcher(messageText);
+            if (m.find()) {
+                messageText = m.replaceAll(rules.get(pattern));
+            }
         }
         message.setText(messageText);
     }
