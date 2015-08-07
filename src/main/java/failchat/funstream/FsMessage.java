ba65b4caa62f443ceaeaf9f34c6424b39b090691
onoderis <onoderis@users.noreply.github.com>
2015-08-07 17:07:46 +0300

    Deserialize funstream messages into FsMessage

--- src/main/java/failchat/funstream/FsMessage.java
+++ src/main/java/failchat/funstream/FsMessage.java
@@ -1,39 +1,75 @@
 package failchat.funstream;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import failchat.core.Source;
 
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FsMessage extends failchat.core.Message {
-    protected  FsChatClient.User from; //for SourceFilter and HighlightHandler
-    protected  FsChatClient.User to; //for HighlightHandler
+    protected User from; //for SourceFilter and HighlightHandler
+    protected User to; //for HighlightHandler
 
-    FsMessage() {}
-
-    FsMessage (FsChatClient.Message message) {
-        this.author = message.getFrom().getName();
-        this.timestamp = message.getTimestamp();
-        this.text = message.getText();
+    FsMessage() {
         this.source = Source.SC2TV;
-
-        this.from = message.getFrom();
-        this.to = message.getTo();
     }
 
     @JsonIgnore
-    public FsChatClient.User getTo() {
+    public User getTo() {
         return to;
     }
 
-    public void setTo(FsChatClient.User to) {
+    @JsonProperty(value = "to")
+    public void setTo(User to) {
         this.to = to;
     }
 
     @JsonIgnore
-    public FsChatClient.User getFrom() {
+    public User getFrom() {
         return from;
     }
 
-    public void setFrom(FsChatClient.User from) {
+    @JsonProperty(value = "from")
+    public void setFrom(User from) {
         this.from = from;
+        this.author = from.getName();
+    }
+
+    @Override
+    @JsonProperty(value = "time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+2")
+    public void setTimestamp(Date timestamp) {
+        super.setTimestamp(timestamp);
+    }
+
+    @Override
+    @JsonProperty(value = "name")
+    public void setAuthor(String author) {
+        super.setAuthor(author);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class User {
+        String name;
+        int id;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
     }
 }
