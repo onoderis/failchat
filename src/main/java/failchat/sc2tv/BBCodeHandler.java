b534b7bf270d804e2c4376cf75f5b030e4c9c0e3
onoderis <onoderis@users.noreply.github.com>
2015-08-10 10:50:27 +0300

    Url bbcode handling fix

--- src/main/java/failchat/sc2tv/BBCodeHandler.java
+++ src/main/java/failchat/sc2tv/BBCodeHandler.java
@@ -14,9 +14,8 @@ import java.util.regex.Pattern;
  */
 
 public class BBCodeHandler implements MessageHandler {
-    private static String announcePattern = "\\[url=.+?\\](.+?)\\[\\/url\\]";
     private static HashMap<Pattern, String> rules = new HashMap<Pattern, String>() {{
-        put(Pattern.compile("\\[url=(.+)\\]\\1\\[\\/url\\]"), "$1"); //иногда ссылки приходят в виде [url=http://xxx.x]http://xxx.x[/url]
+        put(Pattern.compile("\\[url=.+?\\](.+?)\\[\\/url\\]"), "$1"); //иногда ссылки приходят в виде [url=http://xxx.x]http://xxx.x[/url]
         put(Pattern.compile("\\[b\\]"), "");
         put(Pattern.compile("\\[/b\\]"), "");
         put(Pattern.compile("\\[url\\]"), "");
@@ -25,12 +24,6 @@ public class BBCodeHandler implements MessageHandler {
 
     @Override
     public void handleMessage(Message message) {
-        //announce or donation
-        if (message.getAuthor().equals("SC2TV")) {
-            message.setText(message.getText().replaceAll(announcePattern, "$1"));
-        }
-
-        //rules processing
         String messageText = message.getText();
         Set<Pattern> patternSet = rules.keySet();
         for (Pattern pattern : patternSet) {
