8747fd5f860469dc3c6bba22233b13af769e66d6
onoderis <onoderis@users.noreply.github.com>
2015-09-04 14:59:13 +0300

    Added cybergame chat client

--- src/main/java/failchat/handlers/CommonHighlightHandler.java
+++ src/main/java/failchat/handlers/CommonHighlightHandler.java
@@ -1,17 +1,18 @@
-package failchat.goodgame;
+package failchat.handlers;
 
+import failchat.core.Message;
 import failchat.core.MessageHandler;
 import org.apache.commons.lang.StringUtils;
 
-public class GGHighlightHandler implements MessageHandler <GGMessage> {
+public class CommonHighlightHandler implements MessageHandler {
     private String appeal;
 
-    public GGHighlightHandler(String channel) {
+    public CommonHighlightHandler(String channel) {
         this.appeal = channel + ',';
     }
 
     @Override
-    public void handleMessage(GGMessage message) {
+    public void handleMessage(Message message) {
         if (StringUtils.containsIgnoreCase(message.getText(), appeal)) {
             message.setHighlighted(true);
         }
