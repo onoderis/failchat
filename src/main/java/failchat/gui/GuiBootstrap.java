1b1fd3743bd2fd4eed0670efce42a0abb3f4db21
onoderis <onoderis@users.noreply.github.com>
2015-09-06 15:58:18 +0300

    Added failchat icon

--- src/main/java/failchat/gui/GuiBootstrap.java
+++ src/main/java/failchat/gui/GuiBootstrap.java
@@ -1,11 +1,14 @@
 package failchat.gui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.logging.Logger;
 
 public class GuiBootstrap extends Application {
+    public static final Image APP_ICON = new Image(GuiBootstrap.class.getResourceAsStream("/icons/failchat.png"));
+
     private static final Logger logger = Logger.getLogger(GuiBootstrap.class.getName());
 
     public static void main(String[] args) {
