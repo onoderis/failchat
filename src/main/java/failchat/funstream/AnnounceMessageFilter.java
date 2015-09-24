aa9ca1e1aaf8c912572937393a348a7b7fef4a4f
onoderis <onoderis@users.noreply.github.com>
2015-09-24 13:49:36 +0300

    Fix for AnnounceMessageFilter

--- src/main/java/failchat/funstream/AnnounceMessageFilter.java
+++ src/main/java/failchat/funstream/AnnounceMessageFilter.java
@@ -6,11 +6,10 @@ import java.util.logging.Logger;
 
 public class AnnounceMessageFilter implements MessageFilter<FsMessage> {
     private static final Logger logger = Logger.getLogger(AnnounceMessageFilter.class.getName());
-    private static final String ANNOUNCE_MARKER = "начал трансляцию";
 
     @Override
     public boolean filterMessage(FsMessage message) {
-        if (message.getFrom().getId() == 0 && message.getType().equals("donate") && message.getText().contains(ANNOUNCE_MARKER)) {
+        if (message.getType().equals("announce")) {
             logger.fine("Announce message filtered: " + message.getText());
             return true;
         } else {
