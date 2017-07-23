15b71017084aeb19dd0ed572110426fd49fed18a
sph-u <sph-u@users.noreply.github.com>
2017-07-24 00:56:51 +0300

    Move experiments package to test

--- src/test/kotlin/failchat/experiments/OkHttpWsClient.kt
+++ src/test/kotlin/failchat/experiments/OkHttpWsClient.kt
@@ -7,20 +7,26 @@ import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString
+import org.junit.Test
 
-fun main(args: Array<String>) {
-    val client = OkHttpClient.Builder()
-            .retryOnConnectionFailure(true)
-            .build()
+class OkHttpWsClient {
 
-    val reuqest = Request.Builder()
-            .url("ws://chat.goodgame.ru:8081/chat/websocket")
-            .build()
+    @Test
+    fun tryIt() {
+        val client = OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)
+                .build()
 
-    client.newWebSocket(reuqest, Listner())
+        val reuqest = Request.Builder()
+                .url("ws://chat.goodgame.ru:8081/chat/websocket")
+                .build()
+
+        client.newWebSocket(reuqest, Listener())
+    }
+
 }
 
-class Listner : WebSocketListener() {
+class Listener : WebSocketListener() {
     override fun onOpen(webSocket: WebSocket, response: Response) {
         println("onOpen")
 
