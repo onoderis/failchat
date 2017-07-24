35ff2c50363ade34146fe243c23711d7c2a908da
sph-u <sph-u@users.noreply.github.com>
2017-07-24 14:34:38 +0300

    Move code to formatStackTraces()

--- src/main/kotlin/failchat/utils/Strings.kt
+++ src/main/kotlin/failchat/utils/Strings.kt
@@ -14,3 +14,15 @@ inline fun String?.notEmptyOrNull(): String? {
     if (this.isNullOrEmpty()) return null
     return this
 }
+
+fun formatStackTraces(stackTraces: Map<Thread, Array<StackTraceElement>>): String {
+    return stackTraces
+            .map { (thread, stackTraceElements) ->
+                thread.run {
+                    //kotlin's run function
+                    "Thread[name=$name; id=$id; state=${state.name}; isDaemon=$isDaemon; isInterrupted=$isInterrupted;" +
+                            "priority=$priority; threadGroup=${threadGroup.name}]"
+                } + ls + stackTraceElements.joinToString(separator = ls)
+            }
+            .joinToString(separator = ls)
+}
