df841d21d540be39ed258304cd0687e539c29228
onoderis <onoderis@users.noreply.github.com>
2017-07-27 07:37:13 +0300

    Refactor info messages
    Info message filtering moved to web client

--- src/main/kotlin/failchat/core/chat/InfoMessageMode.kt
+++ src/main/kotlin/failchat/core/chat/InfoMessageMode.kt
@@ -1,21 +1,9 @@
 package failchat.core.chat
 
 enum class InfoMessageMode {
-    EVERYWHERE {
-        override fun toString() = "Everywhere"
-    },
-    ON_NATIVE_CLIENT {
-        override fun toString() = "On native client"
-    },
-    NOWHERE {
-        override fun toString() = "Nowhere"
-    };
+    EVERYWHERE,
+    NATIVE_CLIENT,
+    NOWHERE;
 
-    companion object {
-        @JvmStatic
-        fun getValueByString(str: String): InfoMessageMode {
-            return InfoMessageMode.valueOf(str.replace(' ', '_').toUpperCase())
-        }
-    }
-
+    val lowerCaseString: String = this.toString().toLowerCase()
 }
