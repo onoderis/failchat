0b644f2aa578c069357b862feff8d3554c712847
onoderis <onoderis@users.noreply.github.com>
2018-03-03 22:42:36 +0300

    Clean tests

--- src/test/kotlin/failchat/peka2tv/Peka2tvApiTest.kt
+++ src/test/kotlin/failchat/peka2tv/Peka2tvApiTest.kt
@@ -1,24 +1,13 @@
 package failchat.peka2tv
 
 import failchat.config
-import failchat.util.get
-import okhttp3.OkHttpClient
-import org.junit.Ignore
+import failchat.okHttpClient
 import org.junit.Test
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import java.time.Duration
 import kotlin.test.assertTrue
 
-@Ignore
 class Peka2tvApiTest {
 
-    companion object {
-        val log: Logger = LoggerFactory.getLogger(Peka2tvApiTest::class.java)
-        val timeout: Duration = Duration.ofSeconds(5)
-    }
-
-    private val apiClient = Peka2tvApiClient(OkHttpClient(), config.getString("peka2tv.api-url"))
+    private val apiClient = Peka2tvApiClient(okHttpClient, config.getString("peka2tv.api-url"))
 
     @Test
     fun emoticonApiTest() {
@@ -30,14 +19,14 @@ class Peka2tvApiTest {
                         assertTrue { emoticonNode.get("url").isTextual }
                     }
                 }
-                .get(timeout)
+                .join()
     }
 
     @Test
     fun userApiTest() {
         apiClient
                 .findUser("akudji")
-                .get(timeout)
+                .join()
     }
 
 }
