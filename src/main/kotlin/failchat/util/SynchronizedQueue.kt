1bbf4eae4925fe9bf8ea41159cf2ac2ff37b33de
onoderis <onoderis@users.noreply.github.com>
2018-06-15 13:36:55 +0300

    Change SynchronizedQueue synchronization mechanism: use synchronized instead of ReentrantLock

--- src/main/kotlin/failchat/util/SynchronizedQueue.kt
+++ src/main/kotlin/failchat/util/SynchronizedQueue.kt
@@ -1,30 +1,26 @@
 package failchat.util
 
 import java.util.Queue
-import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 class SynchronizedQueue<E>(private val delegate: Queue<E>) : Queue<E> {
-
-    //todo use synchronized
-    private val lock: Lock = ReentrantLock()
-
+    
+    private val mutex = Any()
+    
     override val size: Int
-        get() = lock.withLock { delegate.size }
-    override fun isEmpty() = lock.withLock { delegate.isEmpty() }
-    override fun remove(element: E) = lock.withLock { delegate.remove(element) }
-    override fun remove(): E = lock.withLock { delegate.remove() }
-    override fun iterator() = lock.withLock { delegate.iterator() }
-    override fun retainAll(elements: Collection<E>) = lock.withLock { delegate.retainAll(elements) }
-    override fun peek(): E? = lock.withLock { delegate.peek() }
-    override fun poll(): E? = lock.withLock { delegate.poll() }
-    override fun clear() = lock.withLock { delegate.clear() }
-    override fun add(element: E) = lock.withLock { delegate.add(element) }
-    override fun offer(element: E) = lock.withLock { delegate.offer(element) }
-    override fun contains(element: E) = lock.withLock { delegate.contains(element) }
-    override fun containsAll(elements: Collection<E>) = lock.withLock { delegate.containsAll(elements) }
-    override fun addAll(elements: Collection<E>) = lock.withLock { delegate.addAll(elements) }
-    override fun removeAll(elements: Collection<E>) = lock.withLock { delegate.removeAll(elements) }
-    override fun element(): E = lock.withLock { delegate.element() }
+        get() = synchronized(mutex) { delegate.size }
+    override fun isEmpty() = synchronized(mutex) { delegate.isEmpty() }
+    override fun remove(element: E) = synchronized(mutex) { delegate.remove(element) }
+    override fun remove(): E = synchronized(mutex) { delegate.remove() }
+    override fun iterator() = synchronized(mutex) { delegate.iterator() }
+    override fun retainAll(elements: Collection<E>) = synchronized(mutex) { delegate.retainAll(elements) }
+    override fun peek(): E? = synchronized(mutex) { delegate.peek() }
+    override fun poll(): E? = synchronized(mutex) { delegate.poll() }
+    override fun clear() = synchronized(mutex) { delegate.clear() }
+    override fun add(element: E) = synchronized(mutex) { delegate.add(element) }
+    override fun offer(element: E) = synchronized(mutex) { delegate.offer(element) }
+    override fun contains(element: E) = synchronized(mutex) { delegate.contains(element) }
+    override fun containsAll(elements: Collection<E>) = synchronized(mutex) { delegate.containsAll(elements) }
+    override fun addAll(elements: Collection<E>) = synchronized(mutex) { delegate.addAll(elements) }
+    override fun removeAll(elements: Collection<E>) = synchronized(mutex) { delegate.removeAll(elements) }
+    override fun element(): E = synchronized(mutex) { delegate.element() }
 }
