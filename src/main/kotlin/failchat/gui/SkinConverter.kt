8d1d1567869f2619d21aff30dafd461fbbf8af15
onoderis <onoderis@users.noreply.github.com>
2018-06-27 19:16:01 +0300

    Make SkinConverter return default skin on failed conversion

--- src/main/kotlin/failchat/gui/SkinConverter.kt
+++ src/main/kotlin/failchat/gui/SkinConverter.kt
@@ -1,20 +1,30 @@
 package failchat.gui
 
 import failchat.skin.Skin
+import failchat.util.warn
 import javafx.util.StringConverter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class SkinConverter(skins: List<Skin>) : StringConverter<Skin>() {
 
+    private companion object {
+        val log: Logger = LoggerFactory.getLogger(SkinConverter::class.java)
+    }
+
     private val skinMap: Map<String, Skin> = skins.map { it.name to it }.toMap()
+    private val defaultSkin: Skin = skins.first()
 
     override fun toString(skin: Skin): String {
-        if (skinMap.containsKey(skin.name)) return skin.name
-        else throw RuntimeException("Unknown skin. name=${skin.name}; htmlPath=${skin.htmlPath}") //todo change exception
+        return skin.name
     }
 
     override fun fromString(skinName: String): Skin {
         return skinMap.get(skinName)
-                ?: throw RuntimeException("Unknown skin: $skinName") //todo change exception
+                ?: run {
+                    log.warn { "Unknown skin '$skinName', default skin '${defaultSkin.name}' will be used" }
+                    defaultSkin
+                }
     }
 
 }
