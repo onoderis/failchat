25bcd95be7e5d1c191c6e2aec4d628edffe70799
onoderis <onoderis@users.noreply.github.com>
2018-07-08 10:31:24 +0300

    Use Elements.label() in TwitchEmoticonHandlerTest

--- src/test/kotlin/failchat/twitch/TwitchEmoticonHandlerTest.kt
+++ src/test/kotlin/failchat/twitch/TwitchEmoticonHandlerTest.kt
@@ -6,17 +6,18 @@ import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import failchat.chat.Elements
 import failchat.emoticon.EmoticonFinder
 import org.junit.Test
 
 class TwitchEmoticonHandlerTest {
 
-    val emoticonFinder: EmoticonFinder = mock()
-    val message: TwitchMessage = mock()
-    val firstEmoticon: TwitchEmoticon = mock()
-    val secondEmoticon: TwitchEmoticon = mock()
+    private val emoticonFinder: EmoticonFinder = mock()
+    private val message: TwitchMessage = mock()
+    private val firstEmoticon: TwitchEmoticon = mock()
+    private val secondEmoticon: TwitchEmoticon = mock()
 
-    val handler = TwitchEmoticonHandler(emoticonFinder)
+    private val handler = TwitchEmoticonHandler(emoticonFinder)
 
     @Test
     fun longEmoticonCodeTest() {
@@ -24,11 +25,11 @@ class TwitchEmoticonHandlerTest {
         whenever(message.emotesTag) doReturn("25:0-4,10-14/1902:16-20")
         whenever(emoticonFinder.findById(any(), eq(25L))) doReturn firstEmoticon
         whenever(emoticonFinder.findById(any(), eq(1902L))) doReturn secondEmoticon
-        whenever(message.addElement(any())).thenReturn("\${!0}", "\${!1}", "\${!2}")
+        whenever(message.addElement(any())).thenReturn(Elements.label(0), Elements.label(1), Elements.label(2))
 
         handler.handleMessage(message)
 
-        verify(message).text = "\${!0} 123 \${!1} \${!2} he"
+        verify(message).text = "${Elements.label(0)} 123 ${Elements.label(1)} ${Elements.label(2)} he"
     }
 
     @Test
@@ -37,11 +38,11 @@ class TwitchEmoticonHandlerTest {
         whenever(message.emotesTag) doReturn("1:4-5/2:7-9")
         whenever(emoticonFinder.findById(any(), eq(1L))) doReturn firstEmoticon
         whenever(emoticonFinder.findById(any(), eq(2L))) doReturn secondEmoticon
-        whenever(message.addElement(any())).thenReturn("\${!0}", "\${!1}")
+        whenever(message.addElement(any())).thenReturn(Elements.label(0), Elements.label(1))
 
         handler.handleMessage(message)
 
-        verify(message).text = "123 \${!0} \${!1} 321"
+        verify(message).text = "123 ${Elements.label(0)} ${Elements.label(1)} 321"
     }
 
 }
