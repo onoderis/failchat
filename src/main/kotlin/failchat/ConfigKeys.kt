8464d8aac910e194f8094ca8b005adacab70c8dc
onoderis <onoderis@users.noreply.github.com>
2018-12-18 15:56:08 +0300

    Reset '*.emoticons.last-updated' values if db file not exists
    Also explicitly initialize EmoticonStorage in main()

---
+++ src/main/kotlin/failchat/ConfigKeys.kt
@@ -0,0 +1,5 @@
+package failchat
+
+object ConfigKeys {
+    fun lastUpdatedEmoticons(origin: Origin): String = "${origin.commonName}.emoticons.last-updated"
+}
--- src/main/kotlin/failchat/emoticon/OriginEmoticonStorageFactory.kt
+++ src/main/kotlin/failchat/emoticon/OriginEmoticonStorageFactory.kt
@@ -5,16 +5,20 @@ import failchat.Origin.BTTV_CHANNEL
 import failchat.Origin.BTTV_GLOBAL
 import failchat.Origin.GOODGAME
 import failchat.Origin.PEKA2TV
+import failchat.Origin.TWITCH
 import failchat.twitch.TwitchEmoticonUrlFactory
 import org.mapdb.DB
 
 object OriginEmoticonStorageFactory {
 
-    private val inMemoryOrigins: List<Origin> = listOf(BTTV_GLOBAL, BTTV_CHANNEL, GOODGAME, PEKA2TV)
+    private val commonDbOrigins: List<Origin> = listOf(BTTV_GLOBAL, GOODGAME, PEKA2TV)
+    private val inMemoryOrigins: List<Origin> = listOf(BTTV_CHANNEL)
+
+    val mapdbOrigins: List<Origin> = commonDbOrigins + TWITCH
 
     fun create(db: DB, twitchEmoticonUrlFactory: TwitchEmoticonUrlFactory): List<OriginEmoticonStorage> {
-        return inMemoryOrigins
-                .map { CommonInMemoryEmoticonStorage(it) }
-                .plus(TwitchEmoticonStorage(db, twitchEmoticonUrlFactory))
+        return commonDbOrigins.map { CommonDbEmoticonStorage(db, it) } +
+                inMemoryOrigins.map { CommonInMemoryEmoticonStorage(it) } +
+                TwitchEmoticonStorage(db, twitchEmoticonUrlFactory)
     }
 }
