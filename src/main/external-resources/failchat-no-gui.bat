30811ae14859855c67e4fcc5df34719c595b898a
onoderis <onoderis@users.noreply.github.com>
2018-12-29 13:44:35 +0300

    Add 'enable-console-logging' parameter to failchat-no-gui.bat

--- src/main/external-resources/failchat-no-gui.bat
+++ src/main/external-resources/failchat-no-gui.bat
@@ -1,1 +1,1 @@
-java -Xmx200m -Xms100m -XX:+UseG1GC -jar failchat-${project.version}.jar --no-gui
+java -Xmx200m -Xms100m -XX:+UseG1GC -jar failchat-${project.version}.jar --no-gui --enable-console-logging
