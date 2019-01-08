949465900c5a4a0883df67e9c3ab5cb98d6b4f73
onoderis <onoderis@users.noreply.github.com>
2019-01-08 11:49:57 +0300

    Add colored nicknames support for youtube

---
+++ src/main/kotlin/failchat/youtube/YtColors.kt
@@ -0,0 +1,9 @@
+package failchat.youtube
+
+import javafx.scene.paint.Color
+
+object YtColors {
+    val owner: Color = Color.web("#ffd600")
+    val moderator: Color = Color.web("#5e84f1")
+    val sponsor: Color = Color.web("#2ba640")
+}
