0da9c06d14eaa312524e68934ef2e7e9715730e6
onoderis <onoderis@users.noreply.github.com>
2020-08-16 08:20:11 +0300

    Rebuild and rename transparent-webview-patch.jar

--- src/main/external-resources/failchat.bat
+++ src/main/external-resources/failchat.bat
@@ -1,1 +1,1 @@
-start javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:webview-patch/webview-patch.jar -jar failchat-${project.version}.jar
+start javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar
