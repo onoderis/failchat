0aca51d17a5462f4c2f35ea557c8b3a3802eb48f
onoderis <onoderis@users.noreply.github.com>
2021-02-05 23:11:31 +0300

    Change run scripts

--- src/main/external-resources/failchat.bat
+++ src/main/external-resources/failchat.bat
@@ -1,1 +1,1 @@
-start javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar
+start ./runtime/bin/javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar
---
+++ src/main/external-resources/failchat.sh
@@ -0,0 +1,1 @@
+nohup javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar > /dev/null &
