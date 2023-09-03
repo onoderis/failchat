2ce2ed40896fa9fda08119807ef0bc26d5a47aae
onoderis <onoderis@users.noreply.github.com>
2023-09-03 19:31:40 +0600

    Fix animated twitch emoticons

--- src/main/kotlin/failchat/twitch/TwitchEmoticonUrlFactory.kt
+++ src/main/kotlin/failchat/twitch/TwitchEmoticonUrlFactory.kt
@@ -2,6 +2,6 @@ package failchat.twitch
 
 object TwitchEmoticonUrlFactory {
     fun create(id: String): String {
-        return "https://static-cdn.jtvnw.net/emoticons/v2/$id/static/light/1.0"
+        return "https://static-cdn.jtvnw.net/emoticons/v2/$id/default/light/1.0"
     }
 }
