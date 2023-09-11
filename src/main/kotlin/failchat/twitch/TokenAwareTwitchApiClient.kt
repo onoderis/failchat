39c68dff5ec709d74540e874938a6e9f95fce315
onoderis <onoderis@users.noreply.github.com>
2023-09-11 16:32:23 +0600

    Use mutex for TokenAwareTwitchApiClient

--- src/main/kotlin/failchat/twitch/TokenAwareTwitchApiClient.kt
+++ src/main/kotlin/failchat/twitch/TokenAwareTwitchApiClient.kt
@@ -1,7 +1,8 @@
 package failchat.twitch
 
 import failchat.chat.badge.ImageBadge
-import mu.KLogging
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 /**
  * The [TwitchApiClient] wrapper that:
@@ -14,41 +15,53 @@ class TokenAwareTwitchApiClient(
         private val tokenContainer: HelixTokenContainer
 ) {
 
-    private companion object : KLogging()
+    private val mutex = Mutex() // forbid parallel execution to prevent multiple tokens generation in the same time
 
     suspend fun getUserId(userName: String): Long {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getUserId(userName, it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getUserId(userName, it)
+            }
         }
     }
 
     suspend fun getViewersCount(userName: String): Int {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getViewersCount(userName, it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getViewersCount(userName, it)
+            }
         }
     }
 
     suspend fun getGlobalEmoticons(): List<TwitchEmoticon> {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getGlobalEmoticons(it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getGlobalEmoticons(it)
+            }
         }
     }
 
     suspend fun getFirstLiveChannelName(): String {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getFirstLiveChannelName(it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getFirstLiveChannelName(it)
+            }
         }
     }
 
     suspend fun getGlobalBadges(): Map<TwitchBadgeId, ImageBadge> {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getGlobalBadges(it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getGlobalBadges(it)
+            }
         }
     }
 
     suspend fun getChannelBadges(channelId: Long): Map<TwitchBadgeId, ImageBadge> {
-        return doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
-            twitchApiClient.getChannelBadges(channelId, it)
+        return mutex.withLock {
+            doWithRetryOnAuthError(twitchApiClient, clientSecret, tokenContainer) {
+                twitchApiClient.getChannelBadges(channelId, it)
+            }
         }
     }
 }
