23ac6aa966078c8517ede27ae58b02b5050ac09e
onoderis <onoderis@users.noreply.github.com>
2025-01-25 12:33:53 +0600

    Update failchat.sh

--- src/main/external-resources/failchat.sh
+++ src/main/external-resources/failchat.sh
@@ -1,1 +1,2 @@
-nohup javaw -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar > /dev/null &
+#!/bin/sh
+java -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar > /dev/null 2>&1 &
