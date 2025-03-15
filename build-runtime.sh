1f85c5986b44d05a936d4a574da8b2ba0fb1deb3
onoderis <onoderis@users.noreply.github.com>
2025-03-15 15:38:54 +0600

    Add linux build

--- build-runtime.sh
+++ build-runtime.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+set -eu
+
+JDK_PATH="jdk/bellsoft-jdk11.0.22+12-windows-amd64-full"
+
 jlink --no-header-files \
     --no-man-pages \
     --compress=2 \
     --strip-debug \
     --add-modules java.compiler,java.datatransfer,java.desktop,java.instrument,java.logging,java.management,java.naming,java.sql,java.xml,javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.web,jdk.attach,jdk.jdi,jdk.jsobject,jdk.unsupported,jdk.crypto.ec \
-    --module-path jdk/bellsoft-jdk11.0.22+12-windows-amd64-full/jmods \
+    --module-path "${JDK_PATH}/jmods" \
     --output win-runtime
--- src/main/external-resources/failchat.sh
+++ src/main/external-resources/failchat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar > /dev/null 2>&1 &
+runtime-linux/bin/java -Xmx200m -Xms100m -XX:+UseG1GC -javaagent:java-agents/transparent-webview-patch.jar -jar failchat-${project.version}.jar > /dev/null 2>&1 &
